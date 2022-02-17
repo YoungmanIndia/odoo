83e284195cf61dba39c0c3450348a5d4f4262f2e
Ajay Mahato <ajjayymahato@gmail.com>
2022-02-17 19:05:10 +0530

    Lead lost create prevention module

---
+++ addons/lost_reason_no_create/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Prevent Lost Reason Creation",
+    'summary': """Hides the create option in dropdown of Lost Reason""",
+    'description': """
+        Prevent Lost Reason Creation
+    """,
+    'author': "Youngman India Pvt Ltd",
+    'website': "http://youngman.co.in",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+}
